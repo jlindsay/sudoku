d96f863f43482473812528852d78c34b3d47e396
JOsH Lindsay <jlindsay@JOsHs-MacBook-Pro.local>
2015-04-13 22:10:30 -0700

    refactoring tests

--- src/js/sudoku.jquery.js
+++ src/js/sudoku.jquery.js
@@ -59,60 +59,89 @@
 	        console.log("initGame()");
 	        _seeds = {};
 
+	        var total_numbs = 0;
+	        	total_numbs = (_level == "easy" )? 25 : total_numbs;
+	        	total_numbs = (_level == "easy-ish" )? 20 : total_numbs;
+	        	total_numbs = (_level == "medium" )? 15 : total_numbs;
+	        	total_numbs = (_level == "medium-ish" )? 15 : total_numbs;
+	        	total_numbs = (_level == "hard-ish" )? 10 : total_numbs;
+	        	total_numbs = (_level == "hard" )? 5 : total_numbs;
+	        	console.log("total_numbs:", total_numbs);
+	        
 	        //default level
-	        for(var i=0; i<5; i++){
+	        for(var i=0; i<total_numbs; i++){
 	        	var r_elm = random_range(0,81);
 	        	_seeds[r_elm] = {};
 	        }
 	        
 	      	//lets fetch the random elements, and initilize them.
-	        $(_root).find(".box input[name='edit-input']").each( function( index, elm ){
-	        	var data = input2Data({ target : elm });
+	        $(_root).find(".box input[name='edit_input']").each( function( key, elm ){
+	        	var data = input2data({ target : elm });
 	        	if( _seeds[data.id] )
 	        	{
 	        		_seeds[data.id] = data;
-	        		var r_val = random_range( 1, 9 );
-
-	        		$(data.input_elm).val( r_val );
+	        		setSeedValue( elm );
 					$(data.input_elm).attr("readonly","true");
 					$(data.input_elm).off('keyup');
-
-					testSeedInput( data.input_elm );
 	        	}
 	        });
 
 	    }
 
+	    function setSeedValue( elm )
+	    {
+	    	var r_val = random_range( 1, 9 );
+    		$(elm).val( r_val );
+    		if( !testSeedInput( elm ) ){
+//    			console.log("warning:oops:r_val:already exists:", r_val , ", trying again.");
+    			return setSeedValue( elm );
+    		}
+    		return true;
+	    }
+
 	    function testSeedInput(elm)
 	    {
-	    	var data = input2Data( { target: elm } );
-	        //	console.log("data:", data );
-	       	var results = check( data );
-	        	console.log("testSeedInput:results:", results );
-	        	
-	        	//console.log( "row.status:", results.row.status , "col.status:", results.col.status, "quad.status:");//, results.quad.status );
+	    	var data = input2data( { target: elm } );
+	       	var results = runTests( data );
+//	        	console.log("testSeedInput:col:", results.col.status, "row:", results.row.status, ", quad:", results.quad.status );
+	        if( results.col.status == "error" || results.row.status == "error" || results.quad.status == "error" ){
+	        	return false;
+	        }
 
-	        	//return results;
+	        return true;
 	    }
 
 	    function inputHandler(e)
 	    {
 	            e.preventDefault();
-	        var data = input2Data(e);
-	        var results = check( data );
 
-	        	_history.push( data );
+	        var results = runTests( input2data(e) );
+
+			if( results.col.status == "error" ){
+				console.log("err... huston... col has a duplicate...");
+			}
+			if( results.row.status == "error" ){
+				console.log("err... huston... row has a duplicate...");
+			}
+			if( results.quad.status == "error" ){
+	        	console.log("err... huston... quad has a duplicate...");
+	        }
+	        if( results.col.status == "ok" && results.row.status == "ok" && results.quad.status == "ok" ){
+	        	console.log("ok, were all cool, yo... ");
+	        }
+
+	        _history.push( results );
 	    }
 
-	    function input2Data(e)
+	    function input2data(e)
 	    {
 	        var box_elm    = $(e.target).closest(".box");
 	        var id         = box_elm.find("input[name='id']").val();
 	        var col_id     = box_elm.find("input[name='col_id']").val();
 	        var row_id     = box_elm.find("input[name='row_id']").val();
-	        var quad_col   = box_elm.find("input[name='quad-col']").val();
-	        var quad_id    = box_elm.find("input[name='quad-id']").val();
-	        var input_elm  = box_elm.find("input[name='edit-input']");
+	        var quad_col   = box_elm.find("input[name='quad_col']").val();
+	        var quad_id    = box_elm.find("input[name='quad_id']").val();
+	        var input_elm  = box_elm.find("input[name='edit_input']");
 	        var val        = input_elm.val();
 
 	            val = ( val == "" )? -1 : Number( val );
@@ -129,90 +158,76 @@
 	            }
 	    }
 
-	    function check( data , config )
+	    function runTests( data , config )
 	    {
-//	            console.log( "check:", data );
 	        var results = {};
 	            results.row  = testRow( data.row_id );
 	            results.col  = testCol( data.col_id );
-	            results.quad;// = testQuad( data.quad_id );
+	            results.quad = testQuad( data.quad_id );
 
-//	            console.log( "row:state:", results.row.status , ", col:state:", results.col.status );//,  ", results.quad:", results.quad.status );
 	        return results;
 	    }
-
-	    function testRow( row_id )
-	    {
-	        var elms = $(".game-board .box input[name='row_id'][value='"+ row_id +"'] ");
-	        return testElms( "row", elms );
-	    }
-
-	    function testCol( col_id )
-	    {
-	        var elms = $(".game-board .box input[name='col_id'][value='"+ col_id +"'] ");
-	        return testElms( "column", elms );
-	    }
-
-	    function testQuad( quad_id)
-	    {
-	        var elms = $(".game-board .box input[name='quad-id'][value='"+ quad_id +"'] ");
-	        return testElms( "quadrant", elms );
-	    }
-
-
-	    function html2hash(elms)
-	    {
-	        var hash = {};
-	        $.each( elms, function(index, elm){
-	            var box = $(elm).closest(".box");
-	            var val = $(box).find("input[name='edit-input']").val();
-	                val = ( val == "" )? -1 : Number( val );
-	                hash[val] = hash[val] ? hash[val] : [];
-	                hash[val].push(index);
-	                
-	        });
-//	        console.log("html2hash:hash:", hash );
-	        return hash;
-	    }
-
-	    function testElms( type, elms )
+		
+		function test( type, elms )
 	    {
 	        var hash =  html2hash(elms);
 	        var results = { type: type, hash:hash };
 	        	
-//	        console.log("hash:", hash );
-/*
-	        if( !hash["-1"] && 
-	             hash[1] && hash[2] && hash[3] && hash[4] && 
-	             hash[5] && hash[6] && hash[7] && hash[8] && 
-	             hash[9] ){
-	            //results.status = "success";
-	        	//results.error = null;
-	            //results.description = "success: winner winner, chicken dinner! "+ type + " has all numbers 1-9."; 
-	        }
-*/
-
-	        if( hash["-1"] ){
-	            //results[type].status = "error";
-/*	            
-	            results.errors.push( { code:2, 
-	            					   description : "error: all " + type + " squares are not filled in" });
-*/
-	        }
-
+	        	results.status = "ok";
+	        	results.description = "no worries";
 	        
-	        if( !hash["-1"] && 
-	        	 hash[1] || hash[2] || hash[3] || hash[4] || 
-	        	 hash[5] || hash[6] || hash[7] || hash[8] || 
-	        	 hash[9] )
+	        if( Object.keys(hash).sort().join("") == "123456789" )
 	        {
-	            //results[type].status = "error";
-/*
-	            results.errors.push( { code:1, 
-	            					   description : "error: " + type + " has duplicate values" } );
-*/	        }
+	        	results.status = "ok";
+	        	results.description = "success: winner winner, chicken dinner! "+ type + " is compelte 1-9.";
+	        }else{
+	        	//lets find duplicates & throw an error or something...
+		        $.each( hash, function( key, val ){
+		        	if( val.length > 1 && key != -1 ){
+		        		results.status = "error";
+		        		results.description = String("err... huston... we have a problem, "+ type + "has  duplicate: key:"+ key+ ", at the index:"+ val );
+		        	}
+		        });
+	        }
 
 	        return results;
+	    }
+
+	    /**
+	     * helper(s)
+	     */
+	    function testRow( row_id )
+	    {
+	        return test( "row", $(".game-board .box input[name='row_id'][value='"+ row_id +"'] ") );
+	    }
+
+	    function testCol( col_id )
+	    {
+	        return test( "column", $(".game-board .box input[name='col_id'][value='"+ col_id +"'] ") );
+	    }
+
+	    function testQuad( quad_id)
+	    {
+	        return test( "quadrant", $(".game-board .box input[name='quad_id'][value='"+ quad_id +"'] ") );
+	    }
+
+	    
+		
+		/**
+		 * data-binding
+		 */
+		function html2hash(elms)
+	    {
+	        var hash = {};
+	        $.each( elms, function(index, elm){
+	            var box = $(elm).closest(".box");
+	            var val = $(box).find("input[name='edit_input']").val();
+	                val = ( val == "" )? -1 : Number( val );
+	                hash[val] = hash[val] ? hash[val] : [];
+	                hash[val].push( index );
+	                
+	        });
+	        return hash;
 	    }
 
 	    /**
@@ -222,7 +237,9 @@
 	    {
 	//        console.log("gameBoardHTML()");
 	        var box_html  = boxHTML();
+	        var hud_html = hudHTML();
 
+	        //var html = hud_html;
 	        var html = "";
 	        var row = 0;
 	        var col = 0;
@@ -274,8 +291,11 @@
 	            html += tmpl;
 	        }
 
+	        
+
 	        return html;
 	    }
+
 
 	    function boxHTML()
 	    {
@@ -285,24 +305,33 @@
 	                     <input type='hidden' name='id' value='{{id}}' /> \
 	                     <input type='hidden' name='col_id' value='{{col_id}}' /> \
 	                     <input type='hidden' name='row_id' value='{{row_id}}' /> \
-	                     <input type='hidden' name='quad-col' value={{quad_col}}' /> \
-	                     <input type='hidden' name='quad-id' value={{quad_id}}' /> \
+	                     <input type='hidden' name='quad_col' value='{{quad_col}}' /> \
+	                     <input type='hidden' name='quad_id' value='{{quad_id}}' /> \
 	                 </form>\
-	                 <input type='text' name='edit-input' value='' maxlength='1' onkeypress='return event.charCode >= 49 && event.charCode <= 57' /> \
+	                 <input type='text' name='edit_input' value='' maxlength='1' onkeypress='return event.charCode >= 49 && event.charCode <= 57' /> \
 	            </div>";
 	    }
+
+	    function hudHTML()
+	    {
+	    	return "\
+				<div class='start_screen'>\
+					stuff goes here...\
+				</div>\
+				<div class='finished_screen hidden'></div>\
+	    	";
+	    }
+
 
 	    function random_range( min, max )
 	    {
 	    	return Math.floor(Math.random() * (max - min)) + min;
 	    }
-
 	    
 		/**
 		 * public method(s);
 		 */
 	    return {
-	    	
 	    	init : init,
 	    }
 
